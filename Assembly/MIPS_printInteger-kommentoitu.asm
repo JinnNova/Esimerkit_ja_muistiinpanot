f9f1bf883227a9e48e90be281e1f7affc4694619
Jinn <jozie@mbnet.fi>
2017-08-22 17:13:42 +0300

    MIPS - printing integers example

---
+++ Assembly/MIPS_printInteger-kommentoitu.asm
@@ -0,0 +1,18 @@
+		# MIPS-ohjelma joka printtaa screenille integerin.
+	.data
+numero: .word 29
+		# .word varastoi arvon "29" 32-bittisena "wordina". esim. Integer.
+		# Integer tarvitsee 32 bittia.
+	
+	.text
+main:
+	li $v0, 1
+		# load immidiate $v0 = 1 = system valmistaudu: seuraavaksi printataan word
+	lw $a0, numero
+		# load word "numero" rekisteriin $a0
+	syscall
+		# DO IT!
+	
+	li $v0, 10
+		# ohjelman lopetuskoodi = 10
+	syscall
---
+++ Assembly/MIPS_printInteger.asm
@@ -0,0 +1,11 @@
+	.data
+numero: .word 29
+
+	.text
+main:
+	li $v0, 1
+	lw $a0, numero
+	syscall
+	
+	li $v0, 10
+	syscall
