f6de080567778f9aff356fc58f5aa74a2a8ce0c5
Jinn <jozie@mbnet.fi>
2017-08-22 22:54:45 +0300

    Printing Integers - example
    Päivitetty sisennys

--- Assembly/MIPS_printInteger.asm
+++ Assembly/MIPS_printInteger.asm
@@ -1,11 +1,11 @@
-	.data
-numero: .word 29
+.data
+  numero: .word 29
 
-	.text
-main:
-	li $v0, 1
-	lw $a0, numero
-	syscall
+.text
+  main:
+    li $v0, 1
+    lw $a0, numero
+    syscall
 	
-	li $v0, 10
-	syscall
+    li $v0, 10
+    syscall
