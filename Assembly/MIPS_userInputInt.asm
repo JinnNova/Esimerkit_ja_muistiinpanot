ed1ea96b86cc9be6b86c57cf8d8f7c3e28ad31ec
Jinn <jozie@mbnet.fi>
2017-08-22 17:34:43 +0300

    MIPS - user input (integer) example
    How to take integer as an input

---
+++ Assembly/MIPS_userInputInt.asm
@@ -0,0 +1,25 @@
+	.data
+prompt:  .asciiz "Anna kokonaisluku: "
+message: .asciiz "\nKokonaislukusi on "
+
+	.text
+main:
+	li $v0, 4
+	la $a0, prompt
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	move $t0, $v0
+	
+	li $v0, 4
+	la $a0, message
+	syscall
+	
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	
+	li $v0, 10
+	syscall
