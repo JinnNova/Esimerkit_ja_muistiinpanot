783f0813a81d014e8f146652a66d61612fd615a0
Jinn <jozie@mbnet.fi>
2017-08-24 05:50:57 +0300

    User input (Integer)
    Päivitetty sisennys

--- Assembly/MIPS_userInputInt.asm
+++ Assembly/MIPS_userInputInt.asm
@@ -1,25 +1,25 @@
-	.data
-prompt:  .asciiz "Anna kokonaisluku: "
-message: .asciiz "\nKokonaislukusi on "
+.data
+  prompt:  .asciiz "Anna kokonaisluku: "
+  message: .asciiz "\nKokonaislukusi on "
 
-	.text
-main:
-	li $v0, 4
-	la $a0, prompt
-	syscall
+.text
+  main:
+    li $v0, 4
+    la $a0, prompt
+    syscall
 	
-	li $v0, 5
-	syscall
+    li $v0, 5
+    syscall
 	
-	move $t0, $v0
+    move $t0, $v0
 	
-	li $v0, 4
-	la $a0, message
-	syscall
+    li $v0, 4
+    la $a0, message
+    syscall
 	
-	li $v0, 1
-	move $a0, $t0
-	syscall
+    li $v0, 1
+    move $a0, $t0
+    syscall
 	
-	li $v0, 10
-	syscall
+    li $v0, 10
+    syscall
