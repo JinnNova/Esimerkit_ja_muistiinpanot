2e05d43d8f298add6ea345eea418591367660c76
Jinn <jozie@mbnet.fi>
2017-09-25 23:25:37 +0300

    XORing hex strings and creating a bytearray
    My mini program to XOR two hex strings together and then converting the result into a bytearray (ASCII string)

--- Python3/Hex-XOR-ASCII.py
+++ Python3/Hex-XOR-ASCII.py
@@ -1,17 +1,17 @@
-# Hanna Enqvist 
-# binäärinen XOR operaatio kahdelle HEX:lle ja tuloksen tulostus ASCII:na
+# 1st: XOR operation for two HEX strings
+# 2nd: Converting the integer data into a bytearray (string) to print it in ASCII
+# by. Hanna "Jinn" Enqvist 
 
-
-
+# Put your hex strings here or use these.
 hexString1 = "7b36fd5b41383389dec3be3a9ee577beb51f77c94e330779"
 hexString2 = "3c59892f201854ecaae3da55e98b57d1db3f31bb27576600"
 
-# Muunna HEX stringit integereiksi ja XOR:aa yhteen. 
+# Converting hex (base16) strings to integers and XORing them together.  
 xorred = int(hexString1, 16) ^ int(hexString2, 16)
 
-# ASCII: Decimal (int) bytearrayksi, 1 byte(tavu) = 8 bittiä. length = arrayn pituus tavuina. byteorder = big
-vastaus = xorred.to_bytes((xorred.bit_length() + 7) // 8, 'big').decode()
-print(vastaus)
+# Int To ASCII: Int to bytearray, 1 byte = 8 bits. byteorder = "big".
+result = xorred.to_bytes((xorred.bit_length() + 7) // 8, 'big').decode()
+print(result)
 
-# vastaus = Gotta get down on Friday
+# result = Gotta get down on Friday
 
