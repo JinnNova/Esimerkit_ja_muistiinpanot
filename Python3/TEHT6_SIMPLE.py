0fb8fce0554cab17a09f50ce199a4fe24efafa43
Jinn <jozie@mbnet.fi>
2017-09-25 23:01:20 +0300

    Python3 - Base64 to Hex to Base64 conversion
    My version of a small and simple B64-Hex encoder-decoder in python

--- Python3/TEHT6_SIMPLE.py
+++ Python3/TEHT6_SIMPLE.py
@@ -1,14 +1,18 @@
+# Small program to convert Base64 to Hex and Hex to Base64
+# by. Hanna "Jinn" Enqvist
+
 import base64
 import codecs
 
+#Place your strings here
 base64string = "3q3K/r7vq7rN"
 hexstring = "437468756c68752052276c7965682077676168276e61676c2066687461676e"
 
-vastaus1 = codecs.encode(base64.b64decode(base64string), 'hex').decode()
-print(vastaus1)
-vastaus2 = codecs.encode(codecs.decode(hexstring, 'hex'), 'base64').decode()
-print(vastaus2)
+b64tohex = codecs.encode(base64.b64decode(base64string), 'hex').decode()
+print(b64tohex)
+hextob64 = codecs.encode(codecs.decode(hexstring, 'hex'), 'base64').decode()
+print(hextob64)
 
-#Base64 string to hex: deadcafebeefabbacd
-#Hex string to Base64: Q3RodWxodSBSJ2x5ZWggd2dhaCduYWdsIGZodGFnbg==
+#Base64 string to hex will print: deadcafebeefabbacd
+#Hex string to Base64 will print: Q3RodWxodSBSJ2x5ZWggd2dhaCduYWdsIGZodGFnbg==
 
